b1a7e6206956fa69dbf317a12c9a6b0d7f6d637a
Amanda Silva <amanda328@hotmail.com>
2024-03-13 11:43:20 +0000

    user db rm avatar

--- src/database/knex/migrations/20231123233813_createUsers.js
+++ src/database/knex/migrations/20231123233813_createUsers.js
@@ -3,8 +3,7 @@ exports.up = knex => knex.schema.createTable("users", table => {
     table.text("name");
     table.text("email");
     table.text("password");
-    table.enu("role", ["customer", "admin"]).defaultTo("customer");
-    table.text("avatar").nullable();
+    table.enu("role", ["customer", "admin"], {useNative: true, enumName: "roles" }).notNullable().default("customer");
     
     table.timestamp("created_at").defaultTo(knex.fn.now());
     table.timestamp("updated_at").defaultTo(knex.fn.now());
