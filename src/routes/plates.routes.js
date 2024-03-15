359b0529ea358b7202f87e9994a9ac7a7eba25ee
Amanda Silva <amanda328@hotmail.com>
2024-03-15 11:04:50 +0000

    plate image upload functions

--- src/routes/plates.routes.js
+++ src/routes/plates.routes.js
@@ -1,12 +1,18 @@
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 const PlatesController = require("../controllers/PlatesController");
+const PlateImageController = require("../controllers/PlateImageController");
+
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 const verifyUserAuthorization = require("../middlewares/verifyUserAuthorization");
 
 const platesRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
 const platesController = new PlatesController();
+const plateImageController = new PlateImageController();
 
 platesRoutes.use(ensureAuthenticated);
 
@@ -15,5 +21,6 @@ platesRoutes.put("/:id", verifyUserAuthorization("admin"), platesController.upda
 platesRoutes.get("/:id", platesController.show);
 platesRoutes.delete("/:id", verifyUserAuthorization("admin"), platesController.delete);
 platesRoutes.get("/", platesController.index);
+platesRoutes.patch("/:id/plateimage", verifyUserAuthorization("admin"), upload.single("plateimage"), plateImageController.update);
 
 module.exports = platesRoutes;
